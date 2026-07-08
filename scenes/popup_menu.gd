dcd40bed45690a877863c0c5a6f3ff2d4a228988
Mathieu C. <46716368+Ravenpsycho@users.noreply.github.com>
2026-07-08 14:19:06 +0200

    Update popup_menu.gd

--- scenes/popup_menu.gd
+++ scenes/popup_menu.gd
@@ -1,12 +1,6 @@
 extends PopupMenu
 
-
-const ALL_ROLES: Array = ['Enfant Sauvage', 'Juge Bègue', 'Capitaine', 'Abominable Sectaire', 
-'Bouc Emissaire', 'Voleur', 'Salvateur', 'Loup Garou', 'Renard', 'Assassin', 'Ancien du Village', 
-'Voyante', 'Petite Fille', 'Comédien', 'Simple Villageois', 'Joueur de flûte', 'Demi démon', 
-'Corbeau', 'Cupidon', 'Sorcière', "Montreur d'Ours", 'Loup Garou Blanc', 'Pyromane', 
-'Idiot du Village', 'Plumes du corbeau', 'Nécromancien', "Chevalier à l'épée Rouillée", 
-'Chasseur', 'Ange', 'Chien Loup', 'Dictateur', 'Fossoyeur', 'Grand Méchant Loup']
+const ALL_ROLES: Array = PlayerTile.ALL_ROLES
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
